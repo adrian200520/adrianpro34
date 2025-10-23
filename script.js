a2314ce89a5f7925a3a6613f3885829cb4e1bac9
adrian200520 <agamero0607@gmail.com>
2025-10-23 11:45:02 -0500

    Fix form submission handling and variable names
    Updated event listener to prevent form submission and corrected variable names.

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-const formulario = document.querySelector('#contacto')
+// Seleccionar el formulario
+const formulario = document.querySelector("#contacto")
 
-if (formulario){
-    formulario.addEventListener("submit"), 
-    (event) =>{
-        //evitart que el formulareio se envie
-        event.preventdefault();
+if (formulario) {
+    formulario.addEventListener("submit",
+        (event) => {
+            //Evitar que el formulario se envie
+            event.preventDefault();
 
-        //obtener valores de mi formulario
-        const nombre = document.getElementById("nombre").value;
-        const document = document.getElementById("mensaje").value;
-        const correo = document.getElementById("correo").value;
-        const numero = document.getElementById("telefono").value;
+            //Obtener los valores de mi formulario
+            const nombre = document.getElementById("nombre").value;
+            const descripcion = document.getElementById("mensaje").value;
+            const correo = document.getElementById("Correo").value;
+            const numero = document.getElementById("telefono").value;
 
-        console.log ("nombre", nombre)
-        console.log ("correo", correo)
-        console.log ("numero", numero)
-        console.log ("mensaje es", document)
+            //Imprimir los valores por consola
+            console.log("Nombre", nombre)
+            console.log("Correo", correo)
+            console.log("Celular", numero)
+            console.log("Mensaje es", descripcion)
 
-        alert("Hola " + nombre +
-            "tu correo es " + correo +
-            "tu motivo de contacto es: " + descripcion)
-    }
+            alert("Hola " + nombre +
+                "tu correo es " + correo +
+                "tu motivo de contacto es: " + descripcion)
+        })
 } else {
-console.error("No se pudo encontrar el formulario con el ID #contacto")
+    console.error("No se pudo encontrar el formulario con el ID #contacto")
 }
